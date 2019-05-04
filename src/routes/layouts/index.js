88dc0b8ce5acbfcf34cb1e1836f0e510fc07edaa
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-04 17:28:10 +0700

    added sidebar to the dashboard route layout

--- src/routes/layouts/index.js
+++ src/routes/layouts/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
+import {Icon} from 'semantic-ui-react';
+import Sidebar from './../../components/Sidebar';
 
 export const DashboardLayout = ({component: Component, ...rest}) => {
   return (
     <Route {...rest} render={props => (
       <div className="dashboard">
+        <div className="dashboard__sidebar">
+          <Sidebar>
+            <ul>
+              <li><NavLink to="/"> <Icon name="dashboard" /> Dashboard </NavLink></li>
+            </ul>
+          </Sidebar>
+        </div>
         <div className="dashboard__content">
           <Component {...props} />
         </div>
