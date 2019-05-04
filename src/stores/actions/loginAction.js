3a8c3226944708cc2e1ec0ea5ceb34d520e258e7
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-05 00:49:46 +0700

    created login action

---
+++ src/stores/actions/loginAction.js
@@ -0,0 +1,29 @@
+import Axios from 'axios';
+import {url} from './../../helpers/url';
+import jwt from 'jsonwebtoken';
+
+export const login = (email, password) => dispatch => {
+  dispatch({type: 'LOGIN_REQUEST'})
+
+  return Axios({
+    method: 'POST',
+    url: `${url}/v1/api/login`,
+    data: {
+      'email': email,
+      'password': password
+    }
+  })
+  .then(res => {
+    const decoded = jwt.decode(res.data.token);
+
+    dispatch({
+      type: 'LOGIN_SUCCESS',
+      name: decoded.name,
+      role: decoded.role,
+      token: res.data.token
+    })
+  })
+  .catch(err => {
+    console.log(err.response ? err.response.data.message : err.message);
+  })
+}
