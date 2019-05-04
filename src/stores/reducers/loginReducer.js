fb12bdab20e376bf6976a3c558a4add4c5e66345
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-05 00:50:09 +0700

    updated login reducer

--- src/stores/reducers/loginReducer.js
+++ src/stores/reducers/loginReducer.js
@@ -1,9 +1,29 @@
 const initState = {
+  isFetching: false,
   isLogin: false,
   token: '',
+  name: '',
   role: ''
 }
 
 export const loginReducer = (state = initState, action) => {
+  if(action.type === 'LOGIN_REQUEST'){
+    return {
+      ...state,
+      isFetching: true
+    }
+  }
+
+  if(action.type === 'LOGIN_SUCCESS'){
+    return {
+      ...state,
+      isFetching: false,
+      isLogin: true,
+      token: action.token,
+      name: action.name,
+      role: action.role
+    }
+  }
+
   return state
 }
