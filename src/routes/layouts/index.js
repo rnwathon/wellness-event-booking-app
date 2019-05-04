05cfa4b5952e6707296dcc427c30aa6e9b07a454
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-04 16:23:51 +0700

    updated login layout route

--- src/routes/layouts/index.js
+++ src/routes/layouts/index.js
@@ -16,11 +16,7 @@ export const DashboardLayout = ({component: Component, ...rest}) => {
 export const LoginLayout = ({component: Component, ...rest}) => {
   return (
     <Route {...rest} render={props => (
-      <div className="login">
-        <div className="login__content">
-          <Component {...props} />
-        </div>
-      </div>
+      <Component {...props} />
     )} />
   )
 }
