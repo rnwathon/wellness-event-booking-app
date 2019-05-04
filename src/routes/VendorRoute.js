f1a29dde0f591c0f77137ea953026f0e09351e0f
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-04 19:56:04 +0700

    created vendor route

---
+++ src/routes/VendorRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Switch} from 'react-router-dom';
+import {DashboardLayout} from './layouts';
+import VendorBooking from './components/VendorBooking';
+import VendorEvent from '../components/VendorEvent';
+
+export default function CompanyRoute() {
+  return (
+    <Switch>
+      <DashboardLayout exact path="/" conponent={VendorBooking} />
+      <DashboardLayout exact path="/event" component={VendorEvent} />
+    </Switch>
+  )
+}
