5c431aec02b5fbdb8ca3f48fc53ff33d12a627f0
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-05 00:49:13 +0700

    created login page components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,59 @@
 import React, { Component } from 'react';
-import {Card, Form, Button} from 'semantic-ui-react';
+import {Card, Form, Button, Dimmer, Loader} from 'semantic-ui-react';
+import {connect} from 'react-redux';
+import {login} from './../stores/actions/loginAction';
 
-export default class Login extends Component {
+class Login extends Component {
+  
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.login(this.state.email, this.state.password);
+  }
+
   render() {
+    const {isFetching} = this.props
     return (
       <div className="login">
+        
+        <Dimmer active={isFetching && true}>
+          <Loader />
+        </Dimmer>
+
         <Card>
           <Card.Content className="login__content">
             <Card.Header className="login__header">Login</Card.Header>
             <Card.Meta className="login__meta">Please log in to your account</Card.Meta>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Field>
                 <label> E-mail </label>
                 <input 
+                  name="email"
                   type="email" 
                   placeholder="john@mail.com" 
                   pattern="^(?!\s*$).+"
-                  required />
+                  required 
+                  onChange={this.handleChange}/>
               </Form.Field>
               <Form.Field>
                 <label> Password </label>
                 <input 
+                  name="password"
                   type="password" 
                   placeholder="*****" 
                   pattern="^(?!\s*$).+"
-                  required />
+                  required 
+                  onChange={this.handleChange}/>
               </Form.Field>
               <Form.Field>
                 <Button type="submit" fluid color="blue">Submit</Button>
@@ -36,3 +65,11 @@ export default class Login extends Component {
     )
   }
 }
+
+const mapStateToProps = store => {
+  return {
+    isFetching: store.loginReducer.isFetching
+  }
+}
+
+export default connect(mapStateToProps, {login})(Login);
