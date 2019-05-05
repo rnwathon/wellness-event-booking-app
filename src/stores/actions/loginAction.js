5b227be2d693304cece7be8e45a52291209c236a
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-05 23:32:34 +0700

    integrate alert error trigger

--- src/stores/actions/loginAction.js
+++ src/stores/actions/loginAction.js
@@ -1,9 +1,18 @@
 import Axios from 'axios';
 import {url} from './../../helpers/url';
 import jwt from 'jsonwebtoken';
+import {showErrorAlert} from './modalAction';
 
 export const login = (email, password) => dispatch => {
   dispatch({type: 'LOGIN_REQUEST'})
+
+  // setTimeout(() => {
+  //   dispatch({
+  //     type: 'LOGIN_SUCCESS',
+  //     role: 'vendor',
+  //     token: '123'
+  //   })
+  // }, 500)
 
   return Axios({
     method: 'POST',
@@ -27,5 +36,7 @@ export const login = (email, password) => dispatch => {
       type: 'LOGIN_FAILED',
       message: err.response ? err.response.data.message : err.message
     })
+
+    dispatch(showErrorAlert());
   })
 }
--- src/stores/reducers/index.js
+++ src/stores/reducers/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux';
 import {loginReducer} from './loginReducer';
 import {eventReducer} from './eventReducer';
 import {bookingReducer} from './bookingReducer';
+import {modalReducer} from './modalReducer';
 
 export const rootReducer = combineReducers({
+  modalReducer,
   loginReducer,
   eventReducer,
   bookingReducer
