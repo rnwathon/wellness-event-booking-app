d6ddb8b69009d6f82bf39f2b662865c8fdcc266d
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-05 12:33:37 +0700

    initialized booking reducer

---
+++ src/stores/reducers/bookingReducer.js
@@ -0,0 +1,8 @@
+const initState = {
+  isFetching: false,
+  bookings: []
+}
+
+export const bookingReducer = (state=initState, action) => {
+  return state
+}
--- src/stores/reducers/index.js
+++ src/stores/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {loginReducer} from './loginReducer';
 import {eventReducer} from './eventReducer';
+import {bookingReducer} from './bookingReducer';
 
 export const rootReducer = combineReducers({
   loginReducer,
-  eventReducer
+  eventReducer,
+  bookingReducer
 })
