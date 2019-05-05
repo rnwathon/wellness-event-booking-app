6001da3e3560de0fb2d6a8e343a11d5fe7473a1e
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-05 12:31:40 +0700

    minor refactoring

--- src/components/VendorBooking.js
+++ src/components/VendorBooking.js
@@ -31,7 +31,7 @@ export default class VendorBooking extends Component {
     return (
       <Card className="borderless extra-padding" fluid>
         <Card.Content>
-          <Card.Header blue>Booked Event By Company</Card.Header>
+          <Card.Header>Booked Event By Company</Card.Header>
           <Card.Meta className="mb-2"> Manage your Booked Event by Companies </Card.Meta>
 
           <Table basic="very" padded>
@@ -52,14 +52,14 @@ export default class VendorBooking extends Component {
                       <Table.Cell colSpan="6" textAlign="center"> You Have Not Book Anything Yet.</Table.Cell>
                     </Table.Row>
                   :
-                    this.state.bookedEvents.map(event => {
+                    this.state.bookedEvents.map((event, i) => {
                       return(
-                        <Table.Row>
+                        <Table.Row key={i}>
                           <Table.Cell>{event.name}</Table.Cell>
                           <Table.Cell>{event.company}</Table.Cell>
                           <Table.Cell width={3}>
                             {
-                              event.date.map(date => <Label style={{margin: '0.15em'}}><Icon name="calendar" /> {date} </Label>)
+                              event.date.map((date,i) => <Label key={i} style={{margin: '0.15em'}}><Icon name="calendar" /> {date} </Label>)
                             }
                           </Table.Cell>
                           <Table.Cell width={2}>
