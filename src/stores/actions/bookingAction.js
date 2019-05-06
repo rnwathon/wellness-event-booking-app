78713a70c8d1e6a592e9ec4a04aed74c9cf7c926
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-06 17:41:23 +0700

    created get bookings for vendor action

--- src/stores/actions/bookingAction.js
+++ src/stores/actions/bookingAction.js
@@ -26,3 +26,28 @@ export const getBookingsByCompany = (token) => dispatch => {
     dispatch(showAlert())
   })
 }
+
+export const getBookingsByVendor = (token) => dispatch => {
+  dispatch({type:'GET_BOOKINGS_REQUEST'})
+
+  return Axios({
+    method: 'GET',
+    url: `${url}/v1/api/vendor/booking`,
+    headers: {
+      auth: token
+    }
+  })
+  .then(res => {
+    dispatch({
+      type: 'GET_BOOKINGS_SUCCESS',
+      bookings: res.data.data
+    })
+  })
+  .catch(err => {
+    dispatch({
+      type: 'GET_BOOKINGS_SUCCESS',
+      message: err.response ? err.response.data.message : err.message
+    })
+    dispatch(showAlert())
+  })
+}
