ab834d33ea6eeb56ed3f95bdae78dc5fcc357441
rakhmatnw <rakhmatnashrul@gmail.com>
2019-05-07 01:33:30 +0700

    finished vendor reject booking fuction

--- src/components/VendorBookingReject.js
+++ src/components/VendorBookingReject.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
-import {Modal, Button} from 'semantic-ui-react';
+import {connect} from 'react-redux';
+import {Modal, Button, Form, TextArea, Dimmer, Loader} from 'semantic-ui-react';
+import {respondBooking} from './../stores/actions/bookingAction';
 
-export default class VendorBookingReject extends Component {
+class VendorBookingReject extends Component {
+  state = {
+    remarks : ''
+  }
+
+  handleChange = e => {
+    this.setState({remarks:e.target.value})
+  }
+
+  handleSubmit = e => {
+    this.props.respondBooking(this.props.token, this.props.id, "Rejected", null, this.state.remarks)
+    .then(() => this.props.handleClose())
+  }
+
   render() {
     return (
-      <Modal trigger={<Button color="red">Reject</Button>} centered={false} size="mini">
-        <Modal.Header>Give your remarks</Modal.Header>
+     <Modal trigger={<Button color="red">Reject</Button>} centered={false} size="mini">
+        <Dimmer inverted active={this.props.isFetching && true}>
+          <Loader/>
+        </Dimmer>
+        <Modal.Header>Give Your Remarks</Modal.Header>
         <Modal.Content>
-          <Modal.Description>
-            <p>index ke: {this.props.id}</p>
-          </Modal.Description>
+          <Form>
+            <TextArea rows={3} placeholder="give your remarks/reason..." onChange={this.handleChange}/>
+          </Form>
         </Modal.Content>
         <Modal.Actions>
-          <Button color="green">Submit</Button>
+          <Button color="green" onClick={this.handleSubmit}>Submit</Button>
         </Modal.Actions>
       </Modal>
     )
   }
 }
+
+const mapStateToProps = store => {
+  return{
+    token: store.loginReducer.token,
+    isFetching: store.bookingReducer.isFetching
+  }
+}
+
+export default connect(mapStateToProps, {respondBooking})(VendorBookingReject);
